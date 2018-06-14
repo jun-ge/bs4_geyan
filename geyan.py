6709e2c6ed35b1f9ad7c8431c86ab3558fbc659c
yanwenjun <13072761557@163.com>
2018-06-14 13:09:34 +0800

    添加获取二级目录页面的所有分页网址

--- geyan.py
+++ geyan.py
@@ -3,8 +3,12 @@ import requests
 
 BASE_URL = "https://www.geyanw.com"
 
-#获取主类目的名称和链接
+#获取主页主目录的名称和链接
 def get_main_categories(base_url):
+    """
+    :param base_url: 主页面地址
+    :return: 主页主目录的名称和链接
+    """
     categories_list = []#定义一个空的列表存放，主类目
     res = requests.get(base_url)#通过requests获取响应后的结果（包含，网页内容，状态码，和url信息）
     html_doc = res.content#获取网页的源码
@@ -16,8 +20,30 @@ def get_main_categories(base_url):
         categories_list.append([text, link])#存放
     return categories_list[1:]#剔除第一个，
 
+#获取每个主目录下的所有单页面（获取存在的所有分页）地址
+def get_subdirectorys_urls(url):
+    """
+    :param url: 二级目录页面的地址
+    :return:所有分页地址
+    """
+    urls = []#分页1的网址为二级目录网址
+    res = requests.get(url)
+    html_doc = res.content
+    soup = BeautifulSoup(html_doc, 'lxml')
+    pages = soup.select('.pageinfo strong')[0].get_text()#获取分页的数量
+    for page in range(1, int(pages)+1):
+        urls.append(url + 'list_33_' + str(page) + ".html")#拼接分页网址
+    return urls
 
+#获取子页面中单个分页面的二级目录名称和链接
+def get_subdirectorys(url):
+    """
+    :param url: 分页地址
+    :return:
+    """
+    pass
 
 if __name__ == '__main__':
-    for cate in get_main_categories(BASE_URL):
-        print(cate)#打印出所有类目的名称和网址
+    for main_url in get_main_categories(BASE_URL):
+        for url in get_subdirectorys_urls(main_url[1]):
+            print(url)
